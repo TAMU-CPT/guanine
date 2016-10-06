26ded2ebbbbdc8428d358aa8969738962201780a
Eleni Mijalis <elenimijalis@gmail.com>
2016-10-06 13:15:18 -0500

    django jwt

--- guanine/urls.py
+++ guanine/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url, include
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
 urlpatterns = [
     url(r'', include('base.urls')),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
